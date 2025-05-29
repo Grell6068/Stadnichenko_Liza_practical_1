234d4e542d587cac0759ef9a2bc0a0c89611cf39
Liza Stadnichenko <6068011grell@gmail.com>
2025-05-29 12:10:00 +0300

    practical_1.py

---
+++ practical_1.py
@@ -0,0 +1,9 @@
+print("Hello, Python!")
+print("------")
+name = input("Введіть своє ім'я:")
+print("Вітаю,", name, "!")
+print("------")
+a = input("Перше число:")
+b = input("Друге число:")
+c = a + b
+print("Сума двох чисел:", c)
